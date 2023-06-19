089a4e027ba9ffeef3c1a33b28f0ffd1c91abac5
Sandoct <116573014+Sandoct@users.noreply.github.com>
2023-06-19 17:23:47 +0200

    Update philo.c

--- src/philo.c
+++ src/philo.c
@@ -9,5 +9,28 @@
 /*   Updated: 2023/06/08 17:55:27 by gpouzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "philo.h"
 
+int  philo()
+{
+  create philo;
+  creat thread;
+  philo daily;
+}
 
+void  clear()
+{
+  
+}
+
+int  philo_daily_routine(t_philo philo)
+{
+  pthread_t	death;
+
+  while (!is_dead(philo) && philo.day < philo.data.day)
+	{
+		pthread_create(&death, NULL, check_death, philo);
+    take_forks(philo);
+		philo_eat(philo);
+		pthread_detach(death);
+}
