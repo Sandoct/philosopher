0d3b6d56e94072254d4af260a520ba82b0cb732b
Sandoct <116573014+Sandoct@users.noreply.github.com>
2023-06-21 15:19:04 +0200

    Create tools.c

---
+++ src/tools.c
@@ -0,0 +1,42 @@
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		nb = nb * 10 + (str[i++] - 48);
+	return (nb * sign);
+}
+
+long long	time(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+void	print_mutex(t_philo *philo, char *str)
+{
+	long int	time;
+
+	pthread_mutex_lock(&(philo->info->print));
+	time = time() - philo->info->t_start;
+	if (!philo->info->stop && time >= 0 \
+			&& time <= INT_MAX && !is_dead(philo, 0))
+		printf("%lld %d %s", timestamp() - philo->info->t_start, philo->n, str);
+	pthread_mutex_unlock(&(philo->info->print));
+}
