9d39b23c8f17410bb4c6ce8180bedd470c9b25f4
Sandoct <116573014+Sandoct@users.noreply.github.com>
2023-06-29 11:55:51 +0200

    Update philo.c

--- src/philo.c
+++ src/philo.c
@@ -23,6 +23,65 @@ int	dead(t_philo philo)
 	return (0);
 }
 
+void	*check_death(void *philosophers)
+{
+	t_philo	*philo;
+
+	philo = (t_philo*)philosophers;
+	usleep(philo->data->time_to_die + 1);
+	pthread_mutex_lock(&philo->data->eating);
+	if (!dead(philo) && timestamp() - philo->last_meal >= (long)(philo->data->time_to_die))
+	{
+		pthread_mutex_unlock(&philo->data->eating);
+		print_mutex(philo, " died\n");
+		pthread_mutex_lock(&philo->data->dead);
+		philo->data->died = 1;
+		pthread_mutex_unlock(&philo->data->dead);
+	}
+	pthread_mutex_unlock(&philo->data->eating);
+	return (NULL);
+}
+
+void	philo_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->right_fork));
+	print_mutex(philo, " has taken a fork\n");
+	pthread_mutex_lock((philo->left_fork));
+	print_mutex(philo, " has taken a fork\n");
+	print_mutex(philo, " is eating\n");
+	pthread_mutex_lock(&(philo->data->eating));
+	philo->last_meal = timestamp();
+	pthread_mutex_unlock(&(philo->data->eating));
+	usleep(philo->data->time_to_eat);
+	pthread_mutex_unlock((philo->left_fork));
+	pthread_mutex_unlock(&(philo->right_fork));
+}
+
+void  *philo_daily_routine(void *philosophers)
+{
+	pthread_t	death;
+	t_philo		*philo;
+
+	philo = (t_philo*)philosophers;
+	if (philo->seat % 2 == 0)
+		usleep(philo->data->time_to_eat / 10);
+	else if (philo->seat == philo->data->n_philo)
+		usleep(philo->data->time_to_eat + 1);
+	while (!dead(philo) && !philo->ended)
+	{
+		pthread_create(&death, NULL, check_death, philo);
+		philo_eat(philo);
+		print_mutex(philo, " is sleeping\n");
+		usleep(philo->data->time_to_sleep);
+		print_mutex(philo, " is thinking\n");
+		if (philo->data->n_eat != -1)
+			if (++philo->day >= philo->data->n_eat)
+				philo->ended = 1;
+		pthread_detach(death);
+	}
+	return (NULL);
+}
+
 int  philo(char **arg)
 {
 	t_data	data;
